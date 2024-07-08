4088a0540a756c384d7cdc8240b3f7615cca8424
BOURAIMA <bayizidbour@gmail.com>
2024-07-08 22:04:56 +0200

    création du projet coté backend

---
+++ Back/src/test/java/group/Student_App/Student_App/StudentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package group.Student_App.Student_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
